a3dc93d3d74ae67c5d2334f78f738526d082cb4c
Jim Bruno Goldberg <jimbrunogoldberg@gmail.com>
2018-04-04 09:11:01 +0200

    add ticket and employee models

---
+++ src/app/models/ticket.model.ts
@@ -0,0 +1,10 @@
+
+export class Ticket {
+  public id: number;
+  public name: string;
+  public email: string;
+  public subject: string;
+  public message: string;
+  public type: number;
+  public urgency; number;
+}
